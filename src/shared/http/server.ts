e6e36659f3389d27bc023ead1fe6281edb26d02d
jonatasrmoura <jonatas.rmoura@outlook.com>
2021-02-28 00:02:15 -0300

    feat(ambiente de desenvolvimento)

---
+++ src/shared/http/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors);
+
+app.get('/test', (req, res) => {
+    return res.send('Falaaaa');
+})
+
+app.listen(3333, () => {
+    console.log('Server is running!');
+});
